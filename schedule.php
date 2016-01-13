5948b11e6bf693036fa73ac8eda90990bf6f54fb
Juan Chavez <juan.chavez@west-mec.org>
2016-01-12 18:12:19 -0700

    Made a thing!

--- schedule.php
+++ schedule.php
@@ -1,3 +1,70 @@
+<?php
+require_once('test.php');
+$error = false;
+$success = false;
+
+if(@$_POST['addUser']){
+    /**
+     * New user was submitted. Make sure name and email are present!
+     */
+
+    if(!$_POST['date'])
+    {
+        $error .= '<p>Date is a required field!</p>';
+    }
+
+    if(!$_POST['firstName'])
+    {
+        $error .= '<p>First name is a required field!</p>';
+    }
+
+    if(!$_POST['lastName'])
+    {
+        $error .= '<p>Last name is a required field!</p>';
+    }
+
+    if(!$_POST['burial'])
+    {
+        $error .= '<p>Burial/cremation is a required field!</p>';
+    }
+
+    /**
+     * If we're here...all is well. Process the insert
+     */
+    $stmt = $dbh->prepare('INSERT INTO Schedule (date, firstName, lastName, religion, contactInfo, burial, music, specific_requests, gender) VALUES (:date, :firstName, :lastName, :religion, :contactInfo, :burial, :music, :specific_requests, :gender)');
+    $result = $stmt->execute(
+        array(
+            'date'=>$_POST['date'],
+            'firstName'=>$_POST['firstName'],
+            'lastName'=>$_POST['lastName'],
+            'religion'=>$_POST['religion'],
+            'contactInfo'=>$_POST['contactInfo'],
+            'burial'=>$_POST['burial'],
+            'music'=>$_POST['music'],
+            'specific_requests'=>$_POST['specific_requests'],
+            'gender'=>$_POST['gender']
+        )
+    );
+
+    if($result)
+    {
+        $success = "User " . $_POST['firstName'] . " was successfully saved.";
+    }
+    else
+    {
+        $success = "There was an error saving " . $_POST['email'];
+    }
+}
+
+/**
+ * We'll always want to pull the users to show them in the table
+ */
+$stmt = $dbh->prepare('SELECT * FROM Schedule');
+$stmt->execute();
+$users = $stmt->fetchAll();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -47,32 +114,103 @@
         <!-- END OF NAVIGATION BAR -->
 
         <div id="content">
+            <div class="error">
+                <?php
+                if($error){
+                    echo $error;
+                    echo '<br /><br />';
+                }
+                ?>
+            </div>
+
+
+            <div class="success">
+                <?php
+                if($success){
+                    echo $success;
+                    echo '<br /><br />';
+                }
+                ?>
+            </div>
+
             <br>
             <center>
-                <form>
-                    Date and time of funeral:
-                    <input type="date" name="funeral">&nbsp
-                    <input type="time" name="timeOf">
-                </form>
-                <br>
-                <form action="action_page.php">
+                <form name="addUser" method = "post">
+                    Date:<br>
+                    <input type="date" name="date">
+                    <br>
                     First name:<br>
-                    <input type="text" name="firstname">
+                    <input type="text" name="firstName">
                     <br>
                     Last name:<br>
-                    <input type="text" name="lastname">
+                    <input type="text" name="lastName">
                     <br>
+                    Religion:<br>
+                    <input type="text" name="religion">
+                    <br>
+                    Contact Info of Church:<br>
+                    <input type="text" name="contactInfo">
+                    <br>
+                    Burial<br>
+                    <input type="text" name="burial">
+                    <br>
+                    Music/Hymns:<br>
+                    <input type="text" name="music">
+                    <br>
+                    Specific Requests<br>
+                    <input type="text" name="specific_requests">
+                    <br>
+                    Gender:<br>
+                    <input type="text" name="gender">
+                    <br>
+                    <br>
+                    <button type="submit" name="addUser" value="1"> Submit </button>
                 </form><br>
-                <form action="">
-                    <input type="radio" name="sex" value="male">Male
-                    &nbsp &nbsp &nbsp
-                    <input type="radio" name="sex" value="female">Female
-                    &nbsp &nbsp
-                    <input type="radio" name="sex" value="male">Other
-                </form>
-                <input type="submit" value="Submit">
             </center>
-            <br>
+
+            <?php
+            if($users && count($users)){
+                ?>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Religion</th>
+                        <th>Contact Info</th>
+                        <th>Burial</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Name</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach($users as $user){
+                        ?>
+                        <tr>
+                            <td><?php echo $user['date']?></td>
+                            <td><?php echo $user['firstName']?></td>
+                            <td><?php echo $user['lastName']?></td>
+                            <td><?php echo $user['religion']?></td>
+                            <td><?php echo $user['contactInfo']?></td>
+                            <td><?php echo $user['burial']?></td>
+                            <td><?php echo $user['music']?></td>
+                            <td><?php echo $user['specific_requests']?></td>
+                            <td><?php echo $user['gender']?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <?php
+            }else{
+                echo "There are no users in this system.";
+            }
+            ?>
+
         </div>
         <div id="footer">
             <p style="padding-top: 12px; padding-bottom: 5px">Tyler Cantrell, Juan Chavez, Aiden Navarro, Taylor Spiller [2015]</p>
